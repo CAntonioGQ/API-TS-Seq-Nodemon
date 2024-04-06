ba69b27265fece18016826f310b075805571d843
Cruz Antonio Gastelum Quevedo <macruzgraphic@gmail.com>
2024-04-06 10:16:34 -0700

    Controllers Commit

---
+++ dist/controllers/usuarios.controller.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteUsuario = exports.putUsuario = exports.postUsuario = exports.getUsuario = exports.getUsuarios = void 0;
+const getUsuarios = (req, res) => {
+    res.json({
+        msg: 'getUsuarios'
+    });
+};
+exports.getUsuarios = getUsuarios;
+const getUsuario = (req, res) => {
+    const { id } = req.params;
+    res.json({
+        msg: 'getUsuario',
+        id: id
+    });
+};
+exports.getUsuario = getUsuario;
+const postUsuario = (req, res) => {
+    const { body } = req;
+    res.json({
+        msg: 'postUsuario',
+        body: body
+    });
+};
+exports.postUsuario = postUsuario;
+const putUsuario = (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+    res.json({
+        msg: 'putUsuario',
+        body: body
+    });
+};
+exports.putUsuario = putUsuario;
+const deleteUsuario = (req, res) => {
+    const { id } = req.params;
+    res.json({
+        msg: 'deleteUsuario',
+        id: id
+    });
+};
+exports.deleteUsuario = deleteUsuario;
+//# sourceMappingURL=usuarios.controller.js.map
---
+++ src/controllers/usuarios.controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+
+export const getUsuarios = (req: Request, res: Response) => {
+    res.json({
+        msg: 'getUsuarios'
+    });
+};
+
+export const getUsuario = (req: Request, res: Response) => {
+    const { id } = req.params;
+    res.json({
+        msg: 'getUsuario',
+        id: id
+    });
+};
+
+export const postUsuario = (req: Request, res: Response) => {
+    const { body } = req;
+    res.json({
+        msg: 'postUsuario',
+        body: body
+    });
+};
+
+export const putUsuario = (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { body } = req;
+    res.json({
+        msg: 'putUsuario',
+        body: body
+    });
+};
+
+export const deleteUsuario = (req: Request, res: Response) => {
+    const { id } = req.params;
+    res.json({
+        msg: 'deleteUsuario',
+        id: id
+    });
+};
