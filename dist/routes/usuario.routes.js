18c8db50b4453406f10b1ca00439bc4f1819cab3
Cruz Antonio Gastelum Quevedo <macruzgraphic@gmail.com>
2024-04-06 10:19:24 -0700

    Endpoints Config

--- dist/routes/usuario.routes.js
+++ dist/routes/usuario.routes.js
@@ -1,2 +1,12 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const usuarios_controller_1 = require("../controllers/usuarios.controller");
+const router = (0, express_1.Router)();
+router.get('/', usuarios_controller_1.getUsuarios);
+router.get('/:id', usuarios_controller_1.getUsuario);
+router.post('/', usuarios_controller_1.postUsuario);
+router.put('/:id', usuarios_controller_1.putUsuario);
+router.delete('/:id', usuarios_controller_1.deleteUsuario);
+exports.default = router;
 //# sourceMappingURL=usuario.routes.js.map
