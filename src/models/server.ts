144bbc01eeed806cead98bd89ea618f0ce216aef
Cruz Antonio Gastelum Quevedo <macruzgraphic@gmail.com>
2024-04-06 09:49:40 -0700

    Server Config

---
+++ src/models/server.ts
@@ -0,0 +1,20 @@
+import express, { Application } from 'express'
+
+class Server{
+
+    private app: Application
+    private port: string
+
+    constructor(){
+        this.app = express()
+        this.port = process.env.PORT || '8000'
+    }
+
+    listen() {
+        this.app.listen(this.port, ()=>{
+            console.log('Servidor corriendo en puerto: ' + this.port)
+        })
+    }
+}
+
+export default Server
